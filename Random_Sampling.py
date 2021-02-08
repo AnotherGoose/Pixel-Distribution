46e12d97646eeede72a8bfa42e44aa2fd017b4d2
Jack Maxwell <Jackmaxwell999@gmail.com>
2021-02-08 12:53:22 +0000

    Changed RMSE function

--- Random_Sampling.py
+++ Random_Sampling.py
@@ -5,7 +5,10 @@ import random
 
 #https://stackoverflow.com/questions/17197492/is-there-a-library-function-for-root-mean-square-error-rmse-in-python
 def rmse(predictions, targets):
-    return np.sqrt(((predictions - targets) ** 2).mean())
+    #return np.sqrt(((predictions - targets) **
+    MSE = np.square(np.subtract(targets, predictions)).mean()
+    RMSE = math.sqrt(MSE)
+    return RMSE
 
 def randomSample(img, startX,startY,endX,endY,pixels):
     for i in range(pixels):
@@ -25,7 +28,7 @@ depth = cv2.imread("Depth.png")
 depth = cv2.cvtColor(depth, cv2.COLOR_RGB2GRAY)
 
 #Set Total Pixels
-pix = 50000
+pix = 400000
 roiPor = 80
 backPor = 100 - roiPor
 
@@ -39,8 +42,14 @@ RS = np.zeros((imH, imW), np.uint8)
 
 #========Linear Random Sample Background=========
 rows, cols = RS.shape
+
+if ((rows * cols) < pix):
+    pix = rows * cols
+    print("Error! allocated more pixels than are available")
+
 for i in range(0, pix):
     unused = False
+
     while(unused == False):
         rX = random.randint(0, cols - 1)
         rY = random.randint(0, rows - 1)
@@ -65,9 +74,50 @@ roiTotPix = math.ceil(pix * (roiPor / 100))
 #Round Down background pixels
 backPix = math.floor(pix * (backPor/100))
 
+#Calculate total number of original ROI pixels
+
+pixSumROI = 0
+for i in ROI:
+    x, y, w, h = i
+    pixSumROI += w * h
+
+
+#====================REMOVE===========================
+#ONLY USED TO SHOW THE REGION OF INTEREST
+pCount = 0
+for i in ROI:
+    x, y, w, h = i
+    ROIPort = (w * h)/(pixSumROI)
+    nPixels = round(roiTotPix * ROIPort)
+
+    #Check to make sure doesnt indefinatley loop
+    if((w * h) < nPixels):
+        backPix = backPix + (nPixels - (w * h))
+        nPixels = (w * h)
+
+    for j in range(0, nPixels):
+        unused = False
+        while (unused == False):
+            rX = random.randint(x, x + w - 1)
+            rY = random.randint(y, y + h - 1)
+
+            if AS[rY][rX] == 0:
+                AS[rY][rX] = depth[rY][rX]
+                pCount += 1
+                # AS[rY][rX] = 255
+                unused = True
+            else:
+                #Can indefinetly loop if total number of pixels is surpassed
+                unused = False
+        #img[rY][rX] = 255
+#=====================================================
+
 #Random Sample Background
 #img = randomSample(img, 0, 0, cols-1, rows-1, backPix)
 
+if backPix > ((imW * imH) - pCount):
+    #Make sure not over allocating pixels
+    backPix = (imW * imH) - pCount
 
 for i in range(0, backPix):
     unused = False
@@ -84,35 +134,6 @@ for i in range(0, backPix):
     #img[rY][rX] = 255
 
 #========ROI's Random Sampling=========
-
-#Calculate total number of original ROI pixels
-
-pixSumROI = 0
-for i in ROI:
-    x, y, w, h = i
-    pixSumROI += w * h
-
-
-#====================REMOVE===========================
-#ONLY USED TO SHOW THE REGION OF INTEREST
-for i in ROI:
-    x, y, w, h = i
-    ROIPort = (w * h)/(pixSumROI)
-    nPixels = round(roiTotPix * ROIPort)
-    for j in range(0, nPixels):
-        unused = False
-        while (unused == False):
-            rX = random.randint(x, x + w - 1)
-            rY = random.randint(y, y + h - 1)
-
-            if AS[rY][rX] == 0:
-                AS[rY][rX] = depth[rY][rX]
-                # AS[rY][rX] = 255
-                unused = True
-            else:
-                unused = False
-        #img[rY][rX] = 255
-#=====================================================
 
 #RMSE of total image
 rmseN = rmse(depth, depth)
