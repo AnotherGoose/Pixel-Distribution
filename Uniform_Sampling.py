cae3f8b022d06a8a0a4749512a10abf0cf83662c
Jack Maxwell <Jackmaxwell999@gmail.com>
2021-02-18 20:10:24 +0000

    Added Uniform Adaptive Sampling and seperated uniform distribution into a different function

--- Uniform_Sampling.py
+++ Uniform_Sampling.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import cv2
 from scipy.interpolate import griddata
 
 def nInterp2D(pixels, array):
@@ -37,9 +36,7 @@ def getNewDimensions(nPixels, oWidth, oHeight):
 
     return (nWidth, nHeight)
 
-
 def checkRounding(limit, w, h):
-    tVal = round(w) * round(h)
     if round(w) * round(h) > limit:
         if w * round(h) > limit:
             h = math.floor(h)
@@ -50,25 +47,88 @@ def checkRounding(limit, w, h):
     h = round(h)
     return w, h
 
-def uniformS(img, nPixels):
-    imH, imW = img.shape
+def uniformSpread(oArray, nPoints, nArray):
+    oH, oW = oArray.shape
 
-    US = np.empty((imH, imW))
-    US[:] = np.nan
+    if nPoints > oH * oW:
+        nPoints = oH * oW
 
-    nW, nH = getNewDimensions(nPixels, imW, imH)
-    nW, nH = checkRounding(nPixels, nW, nH)
+    nW, nH = getNewDimensions(nPoints, oW, oH)
+    nW, nH = checkRounding(nPoints, nW, nH)
 
-    stepW = np.linspace(0, imW - 1, nW)
-    stepH = np.linspace(0, imH - 1, nH)
+    stepW = np.linspace(0, oW - 1, nW)
+    stepH = np.linspace(0, oH - 1, nH)
 
     counter = 0
     for i in stepH:
         i = round(i)
         for j in stepW:
             j = round(j)
-            US[i][j] = img[i][j]
+            nArray[i][j] = oArray[i][j]
             counter += 1
 
+    return nArray
+
+def uniformS(img, nPixels):
+    imH, imW = img.shape
+
+    if(nPixels > imH * imW):
+        nPixels = imH * imW
+
+    US = np.empty((imH, imW))
+    US[:] = np.nan
+
+    US = uniformSpread(img, nPixels, US)
+
     uniformNearest = nInterp2D(nPixels, US)
+    return uniformNearest
+
+def nonNan(array):
+    h, w = array.shape
+    counter = 0
+    for i in range(h):
+        for j in range(w):
+            if not math.isnan(array[i][j]):
+                counter += 1
+    return counter
+
+def uniformAS(img, ROI, nPixels, rPort):
+    imH, imW = img.shape
+
+    if(nPixels > imH * imW):
+        nPixels = imH * imW
+
+    AS = np.empty((imH, imW))
+    AS[:] = np.nan
+
+    #Can be an input
+    bPort = 1 - rPort
+
+    rPixels = round(nPixels * rPort)
+    bPixels = round(nPixels * bPort)
+
+    #Pixels Remaining
+    pRem = 0
+
+    #Total pixels in ROI
+    roiSum = 0
+    for r in ROI:
+        x, y, w, h = r
+        roiSum += ((w + 1) * (h + 1))
+
+    for r in ROI:
+        x, y, w, h = r
+        roiPort = ((w + 1) * (h + 1))/roiSum
+        roiPixels = round(roiPort * rPixels)
+
+        #Add on the remainding pixels
+        roiPixels += pRem
+        AS[y:y+h, x:x+w] = uniformSpread(img[y:y+h, x:x+w], roiPixels, AS[y:y+h, x:x+w])
+
+        pRem = roiPixels - nonNan(AS[y:y+h, x:x+w])
+
+    bPixels += pRem
+    AS = uniformSpread(img, bPixels, AS)
+    pUsed = nonNan(AS)
+    uniformNearest = nInterp2D(pUsed, AS)
     return uniformNearest
