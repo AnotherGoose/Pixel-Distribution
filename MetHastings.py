54bce7f0b2427793d6c96672ece9ec9ef40b4bb4
Jack Maxwell <Jackmaxwell999@gmail.com>
2021-02-23 21:37:58 +0000

    Added instance segmentation for metropolis hastings algorithm

--- MetHastings.py
+++ MetHastings.py
@@ -1,24 +1,26 @@
 import numpy as np
 import math
 import random
-import cv2
 import utils
 
 
-def MetHastings(img, ROI, pixels, bConst, roiConst, N):
+def MetHastingsBBox(img, ROI, pixels, bConst, roiConst, N):
+    fMap = utils.createFeatureMapBBox(img, ROI, bConst, roiConst)
+    MH = MetHastings(img, pixels, fMap, N)
+    return MH
+
+def MetHastingsInstance(img, mask, pixels, bConst, roiConst, N):
+    fMap = utils.createFeatureMapInstance(mask, bConst, roiConst)
+    MH = MetHastings(img, pixels, fMap, N)
+    return MH
+
+def MetHastings(img, pixels, fMap, N):
     imH, imW = img.shape
 
     #Define AS value array and Feature Map
     #fMap = np.zeros((imH, imW))
     AS = np.empty((imH, imW))
     AS[:] = np.nan
-
-    if pixels > imH * imW:
-        print("Error: Pixel allocation is too large")
-        pixels = imH * imW
-
-    #Make Feature Map
-    fMap = utils.createFeatureMap(img, ROI, bConst, roiConst)
 
     #Pixels sampled
     pCount = 0
@@ -62,47 +64,21 @@ def MetHastings(img, ROI, pixels, bConst, roiConst, N):
                 AS[nY][nX] = n
                 pCount += 1
 
-    nearestAS = utils.nInterp2D(pixels, AS)
-    #nearestAS = AS
+    #nearestAS = utils.nInterp2D(pixels, AS)
+    nearestAS = AS
     return nearestAS
 
-def RandomWalkMetHastings(img, ROI, pixels, bConst, roiConst, sigma, N):
+def RandomWalkMetHastingsBBox(img, ROI, pixels, bConst, roiConst, sigma, N):
     imH, imW = img.shape
 
     AS = np.empty((imH, imW))
     AS[:] = np.nan
 
     AS = utils.uniformSpread(img, pixels, AS)
-    fMap = utils.createFeatureMap(img, ROI, bConst, roiConst)
+    fMap = utils.createFeatureMapBBox(img, ROI, bConst, roiConst)
 
-    #Split array to quickly go through pixels
-    pUsed = utils.nonNan(AS)
-    values, points = utils.seperateArray(AS, pUsed)
-
-    for i in range(values.size):
-        y, x = points[i]
-        yPrev = y = int(y)
-        xPrev = x = int(x)
-        for j in range(N):
-            xProp = utils.walkIndex(xPrev, imW-1, sigma)
-            yProp = utils.walkIndex(yPrev, imH-1, sigma)
-
-            # Ratio of new point compared to previous on feature map
-            α = min((fMap[yProp][xProp]) / (fMap[yPrev][xPrev]), 1)
-
-            # Random int between 1 and 0
-            r = random.uniform(0, 1)
-            # Check proposal
-            if r < α:
-                # Check if point is used
-                if math.isnan(AS[yProp][xProp]):
-                    yPrev = yProp
-                    xPrev = xProp
-        AS[y][x] = np.nan
-        AS[yPrev][xPrev] = img[yPrev][xPrev]
-    nearestAS = utils.nInterp2D(pixels, AS)
-    #nearestAS = AS
-    return nearestAS
+    RWMH = RandomWalkMetHastings(img, AS, fMap, sigma, N)
+    return RWMH
 
 def RandomWalkMetHastingsInstance(img, mask, pixels, bConst, iConst, sigma, N):
     imH, imW = img.shape
@@ -113,10 +89,10 @@ def RandomWalkMetHastingsInstance(img, mask, pixels, bConst, iConst, sigma, N):
     AS = utils.uniformSpread(img, pixels, AS)
     fMap = utils.createFeatureMapInstance(mask, bConst, iConst)
 
-    RWMH = RandomWalkMetHastingsGen(img, AS, fMap, sigma, N)
+    RWMH = RandomWalkMetHastings(img, AS, fMap, sigma, N)
     return RWMH
 
-def RandomWalkMetHastingsGen(img, AS, fMap, sigma, N):
+def RandomWalkMetHastings(img, AS, fMap, sigma, N):
     imH, imW = img.shape
 
     #Split array to quickly go through pixels
@@ -150,15 +126,22 @@ def RandomWalkMetHastingsGen(img, AS, fMap, sigma, N):
 
 '''
 import cv2
+import os
+import sys
 ROI = np.array([[0, 0, 142, 142]])
 x,y,w,h = ROI[0]
 depth = cv2.imread("Mannequin.png")
-depth = cv2.cvtColor(depth, cv2.COLOR_RGB2GRAY)
-AS = RandomWalkMetHastings(depth, ROI, 10000, 1, 10, 100, 25)
-print(utils.rmse(AS, depth))
-print(utils.rmse(AS[y:y + h, x:x + w], depth[y:y + h, x:x + w]))
-cv2.imshow("UAS", AS)
+depth = cv2.cvtColor(depth, cv2.COLOR_BGR2GRAY)
+pixels = 10000
+RWMH = RandomWalkMetHastingsBBox(depth, ROI, pixels, 1, 10, 100, 25)
+MH = MetHastingsBBox(depth, ROI, pixels, 1, 10, 5)
+#print(utils.rmse(AS, depth))
+#print(utils.rmse(AS[y:y + h, x:x + w], depth[y:y + h, x:x + w]))
+cv2.imshow("RWMH", RWMH)
+cv2.imshow("MH", MH)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
+
+
 
